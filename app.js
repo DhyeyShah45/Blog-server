957da71844e46507a030509f28cc3237bea2eed1
Dhyey Shah <dhyey.shah45@gmail.com>
2023-03-14 16:17:56 +0530

    adding user login routes and code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors')
 const blogRoutes = require('./routes/blogRoutes')
+const userRoutes = require('./routes/userRoutes')
 const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
@@ -9,9 +10,9 @@ const app = express();
 const CONNECTION = process.env.MONGO_URL;
 
 // https://youtu.be/PNtFSVU-YTI - video reference for cors error
-app.use(cors({
-  origin:"http://localhost:3000",
-}))
+// app.use(cors({
+//   origin:"http://localhost:3000",
+// }))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use('/',blogRoutes)
+app.use('/user',userRoutes)
 
 mongoose
   .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
---
+++ controllers/userController.js
@@ -0,0 +1,41 @@
+const User = require("../models/userSchema");
+const jwt = require("jsonwebtoken");
+
+// JWT has a header.payload.sign
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
+
+const signup = async (req, res) => {
+ 
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  login,
+  signup,
+};
